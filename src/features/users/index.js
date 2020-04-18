9ffd34a6a7644907b7ed9f4738af0197c77b0a6d
Ruben Freitas <emanuelfreitas477@gmail.com>
2020-04-19 00:54:19 +0100

    Modal Working

--- src/features/users/index.js
+++ src/features/users/index.js
@@ -1,15 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import debounce from "lodash.debounce";
+import { useInView } from "react-intersection-observer";
 import {
   selectUsersPerLang,
   fetchUsers,
   selectPagesFetch,
-  selectStatus,
 } from "./users-slice";
 import { selectActiveLanguage } from "../settings/settings-slice";
 import { Ol, Li, Thumbnail, Loading, Loader } from "./styles";
-import { useInView } from "react-intersection-observer";
+import UserDetailsModal from "./modal";
 
 export default () => {
   const dispatch = useDispatch();
@@ -17,16 +16,31 @@ export default () => {
   const filteredUsers = useSelector(selectUsersPerLang);
   const pagesFetch = useSelector(selectPagesFetch);
   const [ref, inView] = useInView({ rootMargin: "600px 0px" });
-
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [selectedUser, setSelectedUser] = useState(filteredUsers[0]);
+  console.log("selectedUser", selectedUser);
   useEffect(() => {
     if (inView) dispatch(fetchUsers(activeLanguage, pagesFetch + 1));
   }, [inView]);
 
   return (
     <>
+      {selectedUser && (
+        <UserDetailsModal
+          selectedUser={selectedUser}
+          modalIsOpen={modalIsOpen}
+          closeModal={() => setIsOpen(false)}
+        />
+      )}
       <Ol>
         {filteredUsers.map(({ name, picture }, index) => (
-          <Li key={`${name.first}-${index}`}>
+          <Li
+            key={`${name.first}-${index}`}
+            onClick={() => {
+              setSelectedUser(filteredUsers[index]);
+              setIsOpen(true);
+            }}
+          >
             <Thumbnail src={picture.thumbnail} />
             <p>{name.first}</p>
           </Li>
