d3f837cbef8733d4b35decc3927992a371977637
Rúben Freitas <emanuelfreitas477@gmail.com>
2020-08-01 12:40:59 +0100

    Fix search bar losing search term on navigation

--- src/features/navigation/SearchBar.js
+++ src/features/navigation/SearchBar.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import debounce from "lodash.debounce";
 import { useLocation } from "react-router-dom";
 import { SearchInput } from "./styles";
 import { setSearchTerm } from "./search-slice";
-import { useDispatch } from "react-redux";
-import debounce from "lodash.debounce";
+import { selectSearchTerm } from "../navigation/search-slice";
 
 const SEARCH_DEBOUNCE_INTERVAL = 300;
 
@@ -11,11 +12,13 @@ export default () => {
   const location = useLocation();
   const dispatch = useDispatch();
   const debouncedDispatch = debounce(dispatch, SEARCH_DEBOUNCE_INTERVAL);
+  const searchTerm = useSelector(selectSearchTerm);
 
   return location.pathname === "/" ? (
     <SearchInput
       onChange={(e) => debouncedDispatch(setSearchTerm(e.target.value))}
       placeholder="Search something here"
+      defaultValue={searchTerm}
     />
   ) : null;
 };
